0c1091264ed0199f4cf845ebd9b4a7c1210c42fa
AbdelhamidOuazzou <ouazzouabdelhamid@gmail.com>
2026-05-19 23:22:13 +0100

    docs: add integrated project report and update backend URLs

--- miniaws-frontend/app/src/main/java/com/ouazzou/miniaws/utils/Constants.java
+++ miniaws-frontend/app/src/main/java/com/ouazzou/miniaws/utils/Constants.java
@@ -1,8 +1,8 @@
 package com.ouazzou.miniaws.utils;
 
 public class Constants {
-    public static final String BASE_URL = "https://wed-vocational-fundamentals-armstrong.trycloudflare.com";
-    public static final String WS_URL = "wss://wed-vocational-fundamentals-armstrong.trycloudflare.com/ws/monitoring";
+    public static final String BASE_URL = " https://montreal-exhaust-obvious-acid.trycloudflare.com";
+    public static final String WS_URL = "wss://montreal-exhaust-obvious-acid.trycloudflare.com/ws/monitoring";
 
     // On stockera le token ici après le Login
     public static String CURRENT_TOKEN = "";
